f0a55bfcc61c6ba3fe1712815ff749176748147a
hasan228866 <hasansantuy77@gmail.com>
2024-11-13 14:00:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function kirimPesan() {
 
   // Buat URL WhatsApp
   const nomorWhatsApp = "628895885691"; // Ganti dengan nomor tujuan
-  const pesan = `Halo driver ganteng,aku mau pesen layanan anjem\nNama: ${nama}\nLokasi penjemputan: ${alamatPenjemputan}\nLokasi tujuan: ${alamatTujuan}`;
+  const pesan = `Halo driver ganteng,aku mau pesen layanan anjem\n\nNama: ${nama}\n\nPenjemputan: ${alamatPenjemputan}\n\nTujuan: ${alamatTujuan}`;
   const url = `https://wa.me/${nomorWhatsApp}?text=${encodeURIComponent(pesan)}`;
 
   // Buka URL WhatsApp
