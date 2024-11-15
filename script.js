9b61f12ed10b7fb810f8a843e4403a71408d5a12
hasan228866 <hasansantuy77@gmail.com>
2024-11-15 19:36:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-function kirimPesan() {
-  // Ambil nilai dari input
-  const nama = document.getElementById("nama").value;
-  const alamatPenjemputan = document.getElementById("alamatPenjemputan").value;
-  const alamatTujuan = document.getElementById("alamatTujuan").value;
 
-  // Buat URL WhatsApp
-  const nomorWhatsApp = "628895885691"; // Ganti dengan nomor tujuan
-  const pesan = `Halo driver ganteng,aku mau pesen layanan anjem kamu nihh\n\nNama: ${nama}\n\nPenjemputan: ${alamatPenjemputan}\n\nTujuan: ${alamatTujuan}\n\nHati-hati yaa dijalan,semangatt yaa;)`;
-  const url = `https://wa.me/${nomorWhatsApp}?text=${encodeURIComponent(pesan)}`;
-
-  // Buka URL WhatsApp
-  window.open(url, "_blank");
-}
